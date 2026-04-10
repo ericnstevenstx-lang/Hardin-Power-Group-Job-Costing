c330529613a9fbddd70ecab51da9960f6e048252
ericnstevenstx-lang <ericnstevenstx@gmail.com>
2026-04-10 14:52:57 -0500

    Update index.js

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 
 const LABOR_RATE = 30;
 
